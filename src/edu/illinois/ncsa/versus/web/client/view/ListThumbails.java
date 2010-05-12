c48a909209b5910c4900a6492137614d3bed1b8d
Luigi Marini <lmarini@ncsa.illinois.edu>
2010-05-12 15:45:04 +0000

    Listend for selection events and add to the list of currently selected thumbnails.
    git-svn-id: file:///home/kooper/svn2git/repo/trunk@117 1d209ac1-a921-4c1d-9267-735cbf3ade97

--- src/edu/illinois/ncsa/versus/web/client/view/ListThumbails.java
+++ src/edu/illinois/ncsa/versus/web/client/view/ListThumbails.java
@@ -5,12 +5,18 @@ package edu.illinois.ncsa.versus.web.client.view;
 
 import com.allen_sauer.gwt.dnd.client.PickupDragController;
 import com.allen_sauer.gwt.dnd.client.drop.AbsolutePositionDropController;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
+
+import edu.illinois.ncsa.mmdb.web.client.dispatch.GetPreviews;
+import edu.illinois.ncsa.mmdb.web.client.event.DatasetSelectedEvent;
+import edu.illinois.ncsa.mmdb.web.client.event.DatasetSelectedHandler;
+import edu.illinois.ncsa.mmdb.web.client.ui.PreviewWidget;
 
 /**
  * @author lmarini
@@ -20,10 +26,10 @@ public class ListThumbails extends Composite {
 
 	FlowPanel mainPanel;
 	
-	public ListThumbails(HandlerManager eventBus, PickupDragController dragController) {
+	public ListThumbails(HandlerManager eventBus, final PickupDragController dragController) {
 		mainPanel = new FlowPanel();
 		mainPanel.addStyleName("listThumbnailsPanel");
-		for (int i=0; i<10; i++) {
+		for (int i=0; i<1; i++) {
 			Image image = new Image("images/thumbnail.jpg");
 			image.addStyleName("thumbnail");
 			mainPanel.add(image);
@@ -31,6 +37,18 @@ public class ListThumbails extends Composite {
 		}
 		ScrollPanel scrollPanel = new ScrollPanel(mainPanel);
 		initWidget(scrollPanel);
+		
+		eventBus.addHandler(DatasetSelectedEvent.TYPE, new DatasetSelectedHandler() {
+			
+			@Override
+			public void onDatasetSelected(DatasetSelectedEvent event) {
+				GWT.log("Adding selected dataset to list thumbnails view");
+				PreviewWidget previewWidget = new PreviewWidget(event.getUri(), GetPreviews.SMALL, null);
+				mainPanel.add(previewWidget);
+//				dragController.makeDraggable(previewWidget);
+			}
+		});
+		
 	}
 	
 }
