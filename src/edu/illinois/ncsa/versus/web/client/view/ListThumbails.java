3f5362a3063511e346b1e0ed78a099f62b79ef7c
Luigi Marini <lmarini@ncsa.illinois.edu>
2010-05-13 14:37:50 +0000

    Ability to select and unselect datasets from list datasets table.
    git-svn-id: file:///home/kooper/svn2git/repo/trunk@118 1d209ac1-a921-4c1d-9267-735cbf3ade97

--- src/edu/illinois/ncsa/versus/web/client/view/ListThumbails.java
+++ src/edu/illinois/ncsa/versus/web/client/view/ListThumbails.java
@@ -3,6 +3,9 @@
  */
 package edu.illinois.ncsa.versus.web.client.view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.allen_sauer.gwt.dnd.client.PickupDragController;
 import com.allen_sauer.gwt.dnd.client.drop.AbsolutePositionDropController;
 import com.google.gwt.core.client.GWT;
@@ -16,6 +19,8 @@ import com.google.gwt.user.client.ui.Widget;
 import edu.illinois.ncsa.mmdb.web.client.dispatch.GetPreviews;
 import edu.illinois.ncsa.mmdb.web.client.event.DatasetSelectedEvent;
 import edu.illinois.ncsa.mmdb.web.client.event.DatasetSelectedHandler;
+import edu.illinois.ncsa.mmdb.web.client.event.DatasetUnselectedEvent;
+import edu.illinois.ncsa.mmdb.web.client.event.DatasetUnselectedHandler;
 import edu.illinois.ncsa.mmdb.web.client.ui.PreviewWidget;
 
 /**
@@ -26,15 +31,17 @@ public class ListThumbails extends Composite {
 
 	FlowPanel mainPanel;
 	
+	Map<String, PreviewWidget> previews = new HashMap<String, PreviewWidget>();
+	
 	public ListThumbails(HandlerManager eventBus, final PickupDragController dragController) {
 		mainPanel = new FlowPanel();
 		mainPanel.addStyleName("listThumbnailsPanel");
-		for (int i=0; i<1; i++) {
-			Image image = new Image("images/thumbnail.jpg");
-			image.addStyleName("thumbnail");
-			mainPanel.add(image);
-			dragController.makeDraggable(image);
-		}
+//		for (int i=0; i<1; i++) {
+//			Image image = new Image("images/thumbnail.jpg");
+//			image.addStyleName("thumbnail");
+//			mainPanel.add(image);
+//			dragController.makeDraggable(image);
+//		}
 		ScrollPanel scrollPanel = new ScrollPanel(mainPanel);
 		initWidget(scrollPanel);
 		
@@ -45,7 +52,20 @@ public class ListThumbails extends Composite {
 				GWT.log("Adding selected dataset to list thumbnails view");
 				PreviewWidget previewWidget = new PreviewWidget(event.getUri(), GetPreviews.SMALL, null);
 				mainPanel.add(previewWidget);
-//				dragController.makeDraggable(previewWidget);
+				previews.put(event.getUri(), previewWidget);
+				dragController.makeDraggable(previewWidget);
+			}
+		});
+		
+		eventBus.addHandler(DatasetUnselectedEvent.TYPE, new DatasetUnselectedHandler() {
+			
+			@Override
+			public void onDatasetUnselected(
+					DatasetUnselectedEvent datasetUnselectedEvent) {
+				PreviewWidget previewWidget = previews.get(datasetUnselectedEvent.getUri());
+				mainPanel.remove(previewWidget);
+				previews.remove(datasetUnselectedEvent.getUri());
+				
 			}
 		});
 		
