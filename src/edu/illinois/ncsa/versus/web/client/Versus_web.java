45e11a7a2321918224d4f60193b5dc86100c5547
Luigi Marini <lmarini@ncsa.illinois.edu>
2010-06-11 15:28:23 +0000

    mmdb update. fixed setup of upload servlet.
    git-svn-id: file:///home/kooper/svn2git/repo/trunk@120 1d209ac1-a921-4c1d-9267-735cbf3ade97

--- src/edu/illinois/ncsa/versus/web/client/Versus_web.java
+++ src/edu/illinois/ncsa/versus/web/client/Versus_web.java
@@ -2,7 +2,13 @@ package edu.illinois.ncsa.versus.web.client;
 
 import java.util.List;
 
+import com.allen_sauer.gwt.dnd.client.DragEndEvent;
+import com.allen_sauer.gwt.dnd.client.DragHandler;
+import com.allen_sauer.gwt.dnd.client.DragStartEvent;
 import com.allen_sauer.gwt.dnd.client.PickupDragController;
+import com.allen_sauer.gwt.dnd.client.VetoDragException;
+import com.allen_sauer.gwt.dnd.client.drop.HorizontalPanelDropController;
+import com.allen_sauer.gwt.dnd.client.drop.IndexedDropController;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
@@ -12,11 +18,16 @@ import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.IndexedPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 
+import edu.illinois.ncsa.mmdb.web.client.UploadWidget;
 import edu.illinois.ncsa.mmdb.web.client.dispatch.MyDispatchAsync;
+import edu.illinois.ncsa.mmdb.web.client.event.DatasetUploadedEvent;
+import edu.illinois.ncsa.mmdb.web.client.event.DatasetUploadedHandler;
 import edu.illinois.ncsa.mmdb.web.client.presenter.DatasetTablePresenter;
 import edu.illinois.ncsa.mmdb.web.client.view.DynamicTableView;
 import edu.illinois.ncsa.versus.web.client.event.AddExtractorEvent;
@@ -40,6 +51,8 @@ public class Versus_web implements EntryPoint {
 	private FlowPanel content;
 
 	private HandlerManager eventBus;
+
+	private UploadWidget uploadWidget;
 	
 	public static final MyDispatchAsync dispatchAsync        = new MyDispatchAsync();
 
@@ -49,11 +62,38 @@ public class Versus_web implements EntryPoint {
 	public void onModuleLoad() {
 		eventBus = new HandlerManager(null);
 		// drag and drop
-		PickupDragController dragController = new PickupDragController(RootPanel.get(), false);
+		PickupDragController dragController = new PickupDragController(RootPanel.get(), true);
+		dragController.addDragHandler(new DragHandler() {
+			
+			@Override
+			public void onPreviewDragStart(DragStartEvent event)
+					throws VetoDragException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPreviewDragEnd(DragEndEvent event) throws VetoDragException {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onDragStart(DragStartEvent event) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onDragEnd(DragEndEvent event) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		DockLayoutPanel appPanel = new DockLayoutPanel(Unit.EM);
-		HTML header = new HTML("header");
+		HTML header = new HTML("Versus");
 		header.addStyleName("header");
-		HTML footer = new HTML("footer");
+		HTML footer = new HTML("");
 		footer.addStyleName("footer");
 		HTML navigation = new HTML("files");
 		navigation.addStyleName("navigation");
@@ -81,6 +121,14 @@ public class Versus_web implements EntryPoint {
 		CurrentSelectionsPresenter currentSelectionsPresenter = new CurrentSelectionsPresenter(registryService, eventBus, currentSelectionsView); 
 		currentSelectionsPresenter.go(content);
 		
+		// testing drop targets
+		HorizontalPanel dropTarget = new HorizontalPanel();
+		dropTarget.setBorderWidth(1);
+		dropTarget.setSize("500px", "100px");
+		HorizontalPanelDropController dropController = new HorizontalPanelDropController(dropTarget);
+		dragController.registerDropController(dropController);
+		content.add(dropTarget);
+		
 		// extractors
 		SelectExtractorView selectExtractorView = new SelectExtractorView();
 		SelectExtractorPresenter selectExtractorPresenter = new SelectExtractorPresenter(registryService, eventBus, selectExtractorView);
@@ -93,9 +141,22 @@ public class Versus_web implements EntryPoint {
 
 		// dataset selection
 		DynamicTableView datasetTableView = new DynamicTableView();
-		DatasetTablePresenter datasetTablePresenter = new DatasetTablePresenter(dispatchAsync, eventBus, datasetTableView);
+		final DatasetTablePresenter datasetTablePresenter = new DatasetTablePresenter(dispatchAsync, eventBus, datasetTableView);
 		datasetTablePresenter.bind();
 		content.add(datasetTableView.asWidget());
+		
+		// upload widget
+		uploadWidget = new UploadWidget(false);
+		uploadWidget.addDatasetUploadedHandler(new DatasetUploadedHandler() {
+
+			@Override
+			public void onDatasetUploaded(DatasetUploadedEvent event) {
+				GWT.log("Done uploading file");
+				datasetTablePresenter.refresh();
+			}
+			
+		});
+		content.add(uploadWidget);
 		
 		appPanel.addNorth(header, 2);
 		appPanel.addSouth(footer, 2);
