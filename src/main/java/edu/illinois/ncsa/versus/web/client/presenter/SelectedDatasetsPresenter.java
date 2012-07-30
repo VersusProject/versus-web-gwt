d444a4a3924d7b66beee8577c9b3e195d3165017
Antoine Vandecreme <antoine.vandecreme@nist.gov>
2012-07-30 17:12:03 -0400

    Fix selected datasets display.

--- src/main/java/edu/illinois/ncsa/versus/web/client/presenter/SelectedDatasetsPresenter.java
+++ src/main/java/edu/illinois/ncsa/versus/web/client/presenter/SelectedDatasetsPresenter.java
@@ -11,12 +11,17 @@
  */
 package edu.illinois.ncsa.versus.web.client.presenter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Widget;
+
 import edu.illinois.ncsa.mmdb.web.client.MMDB;
 import edu.illinois.ncsa.mmdb.web.client.TextFormatter;
 import edu.illinois.ncsa.mmdb.web.client.dispatch.GetDatasetsBySet;
@@ -29,41 +34,49 @@ import edu.illinois.ncsa.mmdb.web.client.event.ShowItemEvent;
 import edu.illinois.ncsa.mmdb.web.client.mvp.BasePresenter;
 import edu.illinois.ncsa.mmdb.web.client.presenter.DynamicGridPresenter;
 import edu.illinois.ncsa.mmdb.web.client.presenter.DynamicListPresenter;
-import edu.illinois.ncsa.mmdb.web.client.presenter.DynamicTablePresenter;
 import edu.illinois.ncsa.mmdb.web.client.ui.ContentCategory;
 import edu.illinois.ncsa.mmdb.web.client.view.DynamicGridView;
 import edu.illinois.ncsa.mmdb.web.client.view.DynamicListView;
-import edu.illinois.ncsa.mmdb.web.client.view.DynamicTableView;
+import edu.illinois.ncsa.versus.web.client.presenter.SelectedDatasetsPresenter.Display;
+import edu.illinois.ncsa.versus.web.client.view.SelectedDatasetsView;
 import edu.uiuc.ncsa.cet.bean.DatasetBean;
 import edu.uiuc.ncsa.cet.bean.PersonBean;
-import java.util.HashSet;
 import net.customware.gwt.dispatch.client.DispatchAsync;
 
 /**
  *
  * @author antoinev
  */
-public class SelectedDatasetsPresenter extends BasePresenter<DynamicTablePresenter.Display> {
+public class SelectedDatasetsPresenter extends BasePresenter<Display> {
 
-    private int pageSize = DynamicListView.DEFAULT_PAGE_SIZE;
-    protected String sortKey = "date-desc";
     protected String viewTypePreference;
-    protected String viewType = DynamicTableView.LIST_VIEW_TYPE;
-    protected String sizeType = DynamicTableView.PAGE_SIZE_X1;
-    protected int numberOfPages;
-    protected int currentPage = 1;
+
+    protected String viewType = SelectedDatasetsView.LIST_VIEW_TYPE;
+
     protected BasePresenter<?> viewTypePresenter;
 
-    public SelectedDatasetsPresenter(DispatchAsync dispatch, HandlerManager eventBus, DynamicTablePresenter.Display display) {
-        this(dispatch, eventBus, display, DynamicTableView.LIST_VIEW_TYPE, DynamicTableView.PAGE_SIZE_X1);
+    public interface Display {
+
+        Set<HasValueChangeHandlers<String>> getViewListBox();
+
+        Widget asWidget();
+
+        void setContentView(Widget contentView);
+
+        void setViewType(String viewType);
     }
 
-    public SelectedDatasetsPresenter(DispatchAsync dispatch, HandlerManager eventBus, DynamicTablePresenter.Display display, String defaultview, String defaultsize) {
+    public SelectedDatasetsPresenter(DispatchAsync dispatch,
+            HandlerManager eventBus, Display display) {
+        this(dispatch, eventBus, display, SelectedDatasetsView.LIST_VIEW_TYPE);
+    }
+
+    public SelectedDatasetsPresenter(DispatchAsync dispatch,
+            HandlerManager eventBus, Display display,
+            String defaultview) {
         super(display, dispatch, eventBus);
 
-        sortKey = MMDB.getSessionPreference(getViewSortPreference(), "date-desc");
-        display.setOrder(sortKey);
-        setViewType(MMDB.getSessionPreference(getViewTypePreference(), defaultview), MMDB.getSessionPreference(getViewSizeTypePreference(), defaultsize));
+        setViewType(MMDB.getSessionPreference(getViewTypePreference(), defaultview));
 
         addHandler(RefreshEvent.TYPE, new RefreshHandler() {
 
@@ -79,32 +92,32 @@ public class SelectedDatasetsPresenter extends BasePresenter<DynamicTablePresent
     }
 
     private void getContent() {
-        final HashSet<String> selectedDatasets = new HashSet<String>(MMDB.getSessionState().getSelectedDatasets());
+        final HashSet<String> selectedDatasets = new HashSet<String>(
+                MMDB.getSessionState().getSelectedDatasets());
 
-        service.execute(new GetDatasetsBySet(selectedDatasets), new AsyncCallback<GetDatasetsBySetResult>() {
+        service.execute(new GetDatasetsBySet(selectedDatasets),
+                new AsyncCallback<GetDatasetsBySetResult>() {
 
-            @Override
-            public void onFailure(Throwable caught) {
-                GWT.log("Error getting datasetbean from uri", caught);
-            }
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        GWT.log("Error getting datasetbean from uri", caught);
+                    }
 
-            @Override
-            public void onSuccess(GetDatasetsBySetResult result) {
-                eventBus.fireEvent(new ClearDatasetsEvent());
+                    @Override
+                    public void onSuccess(GetDatasetsBySetResult result) {
+                        eventBus.fireEvent(new ClearDatasetsEvent());
 
-                int index = 0;
-                for (DatasetBean item : result.getDatasets()) {
-                    ShowItemEvent event = new ShowItemEvent();
-                    event.setPosition(index);
-                    addItem(event, item);
-                    index++;
-                    eventBus.fireEvent(event);
-                }
-                eventBus.fireEvent(new NoMoreItemsEvent());
-                final int np = (int) Math.ceil((double) selectedDatasets.size() / getPageSize());
-                setNumberOfPages(np);
-            }
-        });
+                        int index = 0;
+                        for (DatasetBean item : result.getDatasets()) {
+                            ShowItemEvent event = new ShowItemEvent();
+                            event.setPosition(index);
+                            addItem(event, item);
+                            index++;
+                            eventBus.fireEvent(event);
+                        }
+                        eventBus.fireEvent(new NoMoreItemsEvent());
+                    }
+                });
     }
 
     private void addItem(ShowItemEvent event, DatasetBean item) {
@@ -123,170 +136,55 @@ public class SelectedDatasetsPresenter extends BasePresenter<DynamicTablePresent
         event.setType(ContentCategory.getCategory(item.getMimeType(), service));
     }
 
-    /**
-     * Set the total number of pages.
-     *
-     * @param numberOfPages
-     */
-    protected void setNumberOfPages(int numberOfPages) {
-        this.numberOfPages = numberOfPages;
-        display.setTotalNumPages(numberOfPages);
-    }
-
-    protected boolean rememberPageNumber() {
-        return false;
-    }
-
     protected String getPageKey() {
         return this.getClass().getName();
     }
 
-    void setPage(int page) {
-        currentPage = page;
-        display.setPage(page);
-        if (rememberPageNumber()) {
-            GWT.log("Remembering that we're on page " + page);
-            MMDB.getSessionState().setPage(getPageKey(), page); // remember page number in session
-        }
-    }
-
     @Override
     public void bind() {
-        for (HasValueChangeHandlers<Integer> handler : display.getPagingWidget()) {
-            handler.addValueChangeHandler(new ValueChangeHandler<Integer>() {
-
-                @Override
-                public void onValueChange(ValueChangeEvent<Integer> event) {
-                    int page = event.getValue();
-                    GWT.log("User changed page to " + page);
-                    setPage(page);
-                    getContent();
-                }
-            });
-        }
-
-        for (HasValueChangeHandlers<String> handler : display.getSortListBox()) {
-            handler.addValueChangeHandler(new ValueChangeHandler<String>() {
-
-                @Override
-                public void onValueChange(ValueChangeEvent<String> event) {
-                    GWT.log("Sort list box clicked " + event.getValue());
-                    sortKey = event.getValue();
-                    display.setOrder(sortKey);
-                    setPage(1); // FIXME stay on same page?
-                    MMDB.setSessionPreference(getViewSortPreference(), sortKey);
-                    getContent();
-                }
-            });
-        }
-
         for (HasValueChangeHandlers<String> handler : display.getViewListBox()) {
             handler.addValueChangeHandler(new ValueChangeHandler<String>() {
 
                 @Override
                 public void onValueChange(ValueChangeEvent<String> event) {
                     GWT.log("View list box clicked " + event.getValue());
-                    changeViewType(event.getValue(), sizeType);
+                    setViewType(event.getValue());
                     getContent();
                 }
             });
-        }
-
-        for (HasValueChangeHandlers<String> handler : display.getSizeListBox()) {
-            handler.addValueChangeHandler(new ValueChangeHandler<String>() {
-
-                @Override
-                public void onValueChange(ValueChangeEvent<String> event) {
-                    GWT.log("View page size box clicked " + event.getValue());
-                    changeViewType(viewType, event.getValue());
-                    getContent();
-                }
-            });
-        }
-
-        if (rememberPageNumber()) {
-            int rememberedPage = MMDB.getSessionState().getPage(getPageKey());
-            GWT.log("Setting page to remembered page " + rememberedPage);
-            setPage(rememberedPage);
         }
 
         getContent();
     }
 
-    int computeNewPage(int oldPage, int oldPageSize) {
-        return (((oldPage - 1) * oldPageSize) / pageSize) + 1;
-    }
-
     /**
-     * Change the view type. Adjust the page according to the current page on
-     * the previous view type.
-     *
-     * @param viewType
-     * @param sizeType
-     */
-    protected void changeViewType(String viewType, String sizeType) {
-        int oldPage = currentPage;
-        int oldPageSize = pageSize;
-        setViewType(viewType, sizeType);
-        setPage(computeNewPage(oldPage, oldPageSize));
-    }
-
-    /**
-     * Set the view type. Do not adjust the page according to any existing
-     * currentPage value.
+     * Set the view type.
      *
      * @param viewType
      */
-    private void setViewType(String viewType, String sizeType) {
+    private void setViewType(String viewType) {
         this.viewType = viewType;
         display.setViewType(viewType);
         MMDB.setSessionPreference(getViewTypePreference(), viewType);
-        MMDB.setSessionPreference(getViewSizeTypePreference(), sizeType);
         // unbind the existing presenter if any
         if (viewTypePresenter != null) {
             viewTypePresenter.unbind();
         }
-        if (viewType.equals(DynamicTableView.LIST_VIEW_TYPE)) {
+        if (viewType.equals(SelectedDatasetsView.LIST_VIEW_TYPE)) {
             DynamicListView listView = new DynamicListView(service);
-            display.changeListSizeNumbers();
-            display.setSizeType(sizeType);
-            if (sizeType.equals(DynamicTableView.PAGE_SIZE_X2)) {
-                setPageSize(DynamicListView.PAGE_SIZE_X2);
-            } else if (sizeType.equals(DynamicTableView.PAGE_SIZE_X4)) {
-                setPageSize(DynamicListView.PAGE_SIZE_X4);
-            } else {
-                setPageSize(DynamicListView.DEFAULT_PAGE_SIZE);
-            }
-            DynamicListPresenter listPresenter = new DynamicListPresenter(service, eventBus, listView);
+            DynamicListPresenter listPresenter = new DynamicListPresenter(
+                    service, eventBus, listView);
             listPresenter.bind();
             viewTypePresenter = listPresenter;
             display.setContentView(listView);
-        } else if (viewType.equals(DynamicTableView.GRID_VIEW_TYPE)) {
-            display.changeGridSizeNumbers();
-            display.setSizeType(sizeType);
+        } else if (viewType.equals(SelectedDatasetsView.GRID_VIEW_TYPE)) {
             DynamicGridView gridView = new DynamicGridView(service);
-
-            if (sizeType.equals(DynamicTableView.PAGE_SIZE_X2)) {
-                setPageSize(DynamicGridView.PAGE_SIZE_X2);
-            } else if (sizeType.equals(DynamicTableView.PAGE_SIZE_X4)) {
-                setPageSize(DynamicGridView.PAGE_SIZE_X4);
-            } else {
-                setPageSize(DynamicGridView.DEFAULT_PAGE_SIZE);
-            }
-
-            DynamicGridPresenter gridPresenter = new DynamicGridPresenter(service, eventBus, gridView);
+            DynamicGridPresenter gridPresenter = new DynamicGridPresenter(
+                    service, eventBus, gridView);
             gridPresenter.bind();
             viewTypePresenter = gridPresenter;
             display.setContentView(gridView);
         }
-    }
-
-    public int getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
     }
 
     @Override
@@ -299,13 +197,5 @@ public class SelectedDatasetsPresenter extends BasePresenter<DynamicTablePresent
 
     private String getViewTypePreference() {
         return MMDB.DATASET_VIEW_TYPE_PREFERENCE;
-    }
-
-    private String getViewSizeTypePreference() {
-        return MMDB.DATASET_VIEWSIZE_TYPE_PREFERENCE;
-    }
-
-    private String getViewSortPreference() {
-        return MMDB.DATASET_VIEW_SORT_PREFERENCE;
     }
 }
--- src/main/java/edu/illinois/ncsa/versus/web/client/view/SelectedDatasetsView.java
+++ src/main/java/edu/illinois/ncsa/versus/web/client/view/SelectedDatasetsView.java
@@ -11,16 +11,98 @@
  */
 package edu.illinois.ncsa.versus.web.client.view;
 
-import edu.illinois.ncsa.mmdb.web.client.view.DynamicTableView;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import edu.illinois.ncsa.mmdb.web.client.ui.LabeledListBox;
+import edu.illinois.ncsa.versus.web.client.presenter.SelectedDatasetsPresenter;
 
 /**
  *
  * @author antoinev
  */
-public class SelectedDatasetsView extends DynamicTableView {
-    
+public class SelectedDatasetsView extends Composite implements
+        SelectedDatasetsPresenter.Display {
+
+    public static final String LIST_VIEW_TYPE = "list";
+    public static final String GRID_VIEW_TYPE = "grid";
+    private final FlowPanel mainPanel;
+    private final HorizontalPanel topPagingPanel;
+    private final VerticalPanel middlePanel;
+    private final HorizontalPanel bottomPagingPanel;
+    private final LabeledListBox viewOptionsTop;
+    private String viewType;
+    private final LabeledListBox viewOptionsBottom;
+
     public SelectedDatasetsView() {
-        super();
+        mainPanel = new FlowPanel();
+        mainPanel.addStyleName("dynamicTable");
+        initWidget(mainPanel);
+        topPagingPanel = new HorizontalPanel();
+        topPagingPanel.addStyleName("dynamicTableHeader");
+        topPagingPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+        middlePanel = new VerticalPanel();
+        middlePanel.addStyleName("content");
+        bottomPagingPanel = new HorizontalPanel();
+        bottomPagingPanel.addStyleName("dynamicTableHeader");
+        bottomPagingPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+        mainPanel.add(topPagingPanel);
+        mainPanel.add(middlePanel);
+        mainPanel.add(bottomPagingPanel);
+        topPagingPanel.add(new Hyperlink("Select the input datasets.", "listDatasets"));
+        viewOptionsTop = createViewOptions();
+        topPagingPanel.add(viewOptionsTop);
+        topPagingPanel.setCellHorizontalAlignment(viewOptionsTop, 
+                HasHorizontalAlignment.ALIGN_RIGHT);
+        bottomPagingPanel.add(new Hyperlink("Select the input datasets.", "listDatasets"));
+        viewOptionsBottom = createViewOptions();
+        bottomPagingPanel.add(viewOptionsBottom);
+        bottomPagingPanel.setCellHorizontalAlignment(viewOptionsBottom, 
+                HasHorizontalAlignment.ALIGN_RIGHT);
     }
-    
+
+    private LabeledListBox createViewOptions() {
+        LabeledListBox viewOptions = new LabeledListBox("View:");
+        viewOptions.addStyleName("pagingLabel");
+        viewOptions.addItem("List", LIST_VIEW_TYPE);
+        viewOptions.addItem("Grid", GRID_VIEW_TYPE);
+        viewOptions.setSelected(viewType);
+        return viewOptions;
+    }
+
+    @Override
+    public Widget asWidget() {
+        return this;
+    }
+
+    @Override
+    public Set<HasValueChangeHandlers<String>> getViewListBox() {
+        Set<HasValueChangeHandlers<String>> set = new HashSet<HasValueChangeHandlers<String>>();
+        set.add(viewOptionsTop);
+        set.add(viewOptionsBottom);
+        return set;
+    }
+
+    @Override
+    public void setContentView(Widget contentView) {
+        middlePanel.clear();
+        middlePanel.add(contentView);
+
+    }
+
+    @Override
+    public void setViewType(String viewType) {
+        viewOptionsTop.setSelected(viewType);
+        viewOptionsBottom.setSelected(viewType);
+    }
 }
